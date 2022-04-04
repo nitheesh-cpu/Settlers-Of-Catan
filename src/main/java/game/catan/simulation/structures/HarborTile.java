294f7c5a5b58db7e98a8d410f0053202fd84c31c
AadyaSing <90815986+AadyaSing@users.noreply.github.com>
2022-04-04 08:01:37 -0500

    HIIIIIIIIIIIIIIIIIIIIIIIIIIII WASSSSSUPPPPP PEEPS YOLO GANG

---
+++ src/main/java/game/catan/simulation/structures/HarborTile.java
@@ -0,0 +1,22 @@
+package game.catan.simulation.structures;
+
+public class HarborTile extends Tile {
+    private int ratio;
+    private ResourceType type;
+
+    public HarborTile(String r) {
+        super(r);
+    }
+
+    public int getRatio() {
+
+        return ratio;
+    }
+
+    public ResourceType getResource() {
+
+    }
+
+
+
+}
