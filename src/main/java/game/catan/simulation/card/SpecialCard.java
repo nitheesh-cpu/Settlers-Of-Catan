a141bde312298868e5fedf3eb985bb0d56cc6156
FormulaLyric <kay.owoeye@gmail.com>
2022-04-05 07:58:14 -0500

    Added the Card parent class. Also added methods in Development and Special Card classes.

--- src/main/java/game/catan/simulation/card/SpecialCard.java
+++ src/main/java/game/catan/simulation/card/SpecialCard.java
@@ -1,4 +1,22 @@
 package game.catan.simulation.card;
 
-public class SpecialCard {
+import javax.swing.*;
+
+public class SpecialCard extends Card{
+
+    private Player owner;
+    private int victoryPoints;
+
+    public SpecialCard(ImageIcon i, String str)
+    {
+        super(i,str);
+        victoryPoints = 2;
+    }
+
+    public enum SpecialCardType{LARGEST_ARMY, LONGEST_ROAD}
+
+    public void pickCard()
+    {
+        //should randomly pick a card in the enum
+    }
 }
