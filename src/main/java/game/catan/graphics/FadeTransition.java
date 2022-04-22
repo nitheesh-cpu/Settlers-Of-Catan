e016072102e4a594c9f2a2d39933c8e70e7d447b
nitheeshcpu <nitishkoda@outlook.com>
2022-04-21 22:54:29 -0500

    Fixed tile structure and road storage

--- src/main/java/game/catan/graphics/FadeTransition.java
+++ src/main/java/game/catan/graphics/FadeTransition.java
@@ -10,7 +10,7 @@ public class FadeTransition {
     public static void applyFadeTransition(Node node, Duration duration, EventHandler<ActionEvent> event) {
         javafx.animation.FadeTransition fadeIn = new javafx.animation.FadeTransition(duration, node);
         fadeIn.setCycleCount(1);
-        fadeIn.setFromValue(0.2);
+        fadeIn.setFromValue(0.75);
         fadeIn.setToValue(1);
         fadeIn.setAutoReverse(true);
         fadeIn.setOnFinished(event);
@@ -18,7 +18,7 @@ public class FadeTransition {
         javafx.animation.FadeTransition fadeOut = new javafx.animation.FadeTransition(duration, node);
         fadeOut.setCycleCount(1);
         fadeOut.setFromValue(1);
-        fadeOut.setToValue(0.2);
+        fadeOut.setToValue(0.75);
         fadeOut.setAutoReverse(true);
 
         fadeOut.play();
