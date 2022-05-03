04c205abb69bcfd9070980fc024cc2c154e6f124
nitheesh-cpu <nitishkoda@outlook.com>
2022-05-03 08:06:35 -0500

    added domestic trading confirmation

--- src/main/java/game/catan/graphics/HelpController.java
+++ src/main/java/game/catan/graphics/HelpController.java
@@ -1,7 +1,11 @@
 package game.catan.graphics;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class HelpController {
 
@@ -42,5 +46,18 @@ public class HelpController {
     @FXML //for top menu minimize button
     void minimizeClicked(MouseEvent event) {
         HelloApplication.helpStage.setIconified(true);
+    }
+
+    @FXML
+    void download(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("All Files", "*.*"));
+        String fileLocationPath = fileChooser.showSaveDialog(HelloApplication.helpStage).getAbsolutePath();
+        File fileLocation = new File("https://www.catan.com/sites/prod/files/2021-06/catan_base_rules_2020_200707.pdf");
+        File fileLocation1 = new File(fileLocation.getName());
+        if(fileLocation.exists()){
+            fileLocationPath = fileLocation1.getAbsolutePath();
+        }
     }
 }
